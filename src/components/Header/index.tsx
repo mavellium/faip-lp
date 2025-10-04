8f7f7317803ede99a218ca90fbed1a9914ba07f1
viniciustmota <motaviny140@gmail.com>
2025-10-04 19:46:30 -0300

    fix: responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { useCallback } from "react";
 
 export default function Header() {
   // Função scroll suave
-  const xloothScrollTo = useCallback((targetY: number, duration = 600) => {
+  const lgoothScrollTo = useCallback((targetY: number, duration = 600) => {
     const startY = window.scrollY || window.pageYOffset;
     const distanceY = targetY - startY;
     let startTime: number | null = null;
@@ -38,7 +38,7 @@ export default function Header() {
     const el = document.getElementById("section-2-form");
     if (el) {
       const top = el.getBoundingClientRect().top + window.scrollY;
-     xloothScrollTo(top, 700);
+     lgoothScrollTo(top, 700);
     }
   };
 
@@ -64,15 +64,15 @@ export default function Header() {
       ></div>
 
       {/* Logos e conteúdo */}
-      <div className="flex gap-18 xl:gap-7 w-full xl:w-auto flex-col h-screen relative z-30 p-2 pt-6 pb-6 xl:pl-20">
-        <div className="flex gap-10 xl:justify-start justify-center">
+      <div className="flex gap-18 md:gap-7 w-full md:w-auto flex-col h-screen relative z-30 p-2 pt-6 pb-6 md:pl-20">
+        <div className="flex gap-8 lg:gap-10 md:justify-start justify-center">
         <Image
           src="/faip-logo.png"
           alt="Logo FAIP Marília"
           width={102}
           height={50}
           priority
-          className="w-[100px] xl:w-[7.2vw] h-auto"
+          className="w-[25vw] sm:w-[20vw] md:w-[10vw] lg:w-[7.2vw] h-auto"
         />
 
         <Image
@@ -81,35 +81,36 @@ export default function Header() {
           width={200}
           height={50}
           priority
-          className="w-[180px] xl:w-[15.5vw] h-auto"
+          className="w-[40vw] md:w-[20vw] lg:w-[15.5vw] h-auto"
         />
         </div>
 
         {/* Texto e botão */}
-        <div className="flex flex-col items-center xl:items-start xl:justify-center h-full text-center xl:text-start xl:w-[25vw]">
-          <h1 className="text-[clamp(1.4rem,2.13vw,20rem)] font-bold max-w-full xl:max-w-[60em] text-white drop-shadow-xl">
-            <span className="text-[clamp(1.4rem,3vw,3em)] font-bold">Faça sua matrícula</span>
+        <div className="flex flex-col items-center lg:items-start md:justify-center h-full text-center lg:text-start lg:w-[26vw]">
+          <h1 className="text-[clamp(1.55rem,5.6vw,20rem)] md:text-[clamp(1.43rem,2.13vw,20rem)] font-bold max-w-full lg:max-w-[60em] text-white drop-shadow-lg">
+            <span className="text-[clamp(2.21rem,8vw,4em)] md:text-[clamp(2.025rem,3vw,4em)] font-bold">Faça sua matrícula</span>
             <br></br>
             e garanta bolsa de até
           </h1>
 
-          <span className="flex items-baseline text-[clamp(1.4rem,15vw,20rem)] w-full justify-center font-bold mt-2 leading-none text-white drop-shadow-xl h-[25vh]">
+          <span className="flex items-baseline text-[clamp(10rem,46vw,20rem)] md:text-[clamp(10rem,15vw,20rem)] w-full justify-center font-bold mt-2 leading-none text-white drop-shadow-lg h-[clamp(15vh,40vw,50vh)] sm:h-[30vh] md:h-[18vh] lg:h-[18vh] xl:h-[25vh]">
             60
-            <span className="text-[40px] xl:text-[80px] md:text-[50px] ml-2">%</span>
+            <span className="text-[56px] sm:text-[64px] lg:text-[80px] md:text-[50px] ml-2">%</span>
           </span>
 
-          <h2 className="text-[clamp(1.4rem,1.48vw,20rem)] font-bold max-w-full xl:max-w-xl mb-4 text-white drop-shadow-xl">
-            <span className="text-[clamp(1.4rem,2.1vw,20rem)]">e comece o ano garantindo </span>
+          <h2 className="text-[clamp(1.09rem,3.95vw,20rem)] md:text-[clamp(0.992rem,1.48vw,20rem)] font-bold max-w-full lg:max-w-lg mb-4 text-white drop-shadow-lg">
+            <span className="text-[clamp(1.55rem,5.6vw,20rem)] md:text-[clamp(1.41rem,2.1vw,20rem)]">e comece o ano garantindo </span>
+            <br></br>
             seu sonho de ter o seu diploma!
           </h2>
 
-          <h3 className="text-[clamp(1.0rem,1.1vw,20rem)] mb-6 text-white drop-shadow-xl">
+          <h3 className="text-[clamp(0.81rem,2.94vw,20rem)] md:text-[clamp(0.736rem,1.1vw,20rem)] md:text-[clamp(0.736rem,1.1vw,20rem)] mb-6 text-white drop-shadow-lg">
             Cursos 100% presenciais desde o 1° Semestre!
           </h3>
 
           {/* Link com handler de scroll suave */}
           <a href="#form" onClick={handleClick}>
-            <button className="bg-white border-2 border-[#045C3A] text-[#045C3A] cursor-pointer rounded-[12px] h-12 w-[25vw] px-6 font-semibold shadow-xl shadow-green-500/40 backdrop-blur xl transition-all duration-300 hover:bg-[#045C3A] hover:text-white transform hover:-translate-y-1">
+            <button className="bg-white border-2 border-[#045C3A] text-[clamp(1rem,3.7vw,20rem)] md:text-[clamp(0.85rem,1.2vw,20rem)] text-[#045C3A] cursor-pointer rounded-[12px] h-12 w-full lg:w-[25vw] px-6 font-semibold shadow-lg shadow-green-500/40 backdrop-blur transition-all duration-300 hover:bg-[#045C3A] hover:text-white transform hover:-translate-y-1">
               QUERO MINHA BOLSA AGORA
             </button>
           </a>
