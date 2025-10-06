aa7328dcb79f92e56c4b6fcbcef995962c059218
Lu <muchasgraccias@gmail.com>
2025-10-06 00:07:26 -0300

    fix: font-size responsivos Headline

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -43,7 +43,7 @@ export default function Header() {
   };
 
   return (
-    <section className="relative w-full h-screen flex flex-col xl:justify-start justify-center items-center xl:items-start overflow-hidden">
+    <section className="relative w-full h-screen xl:h-[50em] flex flex-col xl:justify-start justify-center items-center xl:items-start overflow-hidden">
 
       <video
         autoPlay
@@ -64,7 +64,7 @@ export default function Header() {
       ></div>
 
 
-      <div className="flex gap-18 md:gap-7 w-9/10 sm:w-3/4 xl:w-1/3 flex-col h-screen relative z-30 p-2 pt-6 pb-6 xl:pl-20" id="secao">
+      <div className="flex gap-7 md:gap-7 w-9/10 sm:w-3/4 xl:w-1/3 flex-col h-screen relative z-30 p-2 pt-6 pb-6 xl:pl-20" id="secao">
         <div className="flex gap-8 xl:gap-10 xl:justify-start justify-center">
           <Image
             src="/faip-logo.webp"
@@ -72,7 +72,7 @@ export default function Header() {
             width={166}
             height={75}
             priority
-            className="w-1/3 h-auto"
+            className="w-1/4 h-auto"
             id="logo_1"
           />
 
@@ -88,28 +88,28 @@ export default function Header() {
         </div>
 
         <div className="flex flex-col items-center xl:items-start md:justify-center h-full text-center xl:text-start w-full lg:max-w-[100%] xl:w-[26vw]" id="secao_headline">
-          <h1 className="text-[clamp(1.55rem,5.6vw,20rem)] xl:text-[clamp(1.43rem,2.13vw,20rem)] font-bold max-w-full xl:max-w-[60em] text-white drop-shadow-xl " id="h1_2">
-            <span className="text-[clamp(2.21rem,8vw,4em)] xl:text-[clamp(2.025rem,3vw,4em)] font-bold" id="h1_1">Faça sua matrícula</span>
+          <h1 className="text-[clamp(1.55rem,5.6vw,20rem)] xl:text-[clamp(1.43rem,2.23vw,20rem)] font-bold max-w-full xl:max-w-[60em] text-white drop-shadow-xl " id="h1_2">
+            <span className="text-[clamp(2.21rem,8vw,4em)] xl:text-[clamp(2.025rem,2.83vw,4em)] font-bold" id="h1_1">Faça sua matrícula</span>
             <br></br>
             e garanta bolsa de até
           </h1>
 
-          <span className="flex items-baseline text-center text-[clamp(10rem,46vw,30rem)] xl:text-[clamp(10rem,15vw,20rem)] w-full justify-center font-bold mt-10 text-white drop-shadow-xl leading-[0.7]  h-fit" id="h3_60">
+          <span className="flex items-baseline text-center text-[clamp(10rem,46vw,30rem)] xl:text-[clamp(10rem,13.60vw,20rem)] w-full justify-center font-bold mt-10 text-white drop-shadow-xl leading-[0.7]  h-fit" id="h3_60">
             60
             <span className="text-[56px] sm:text-[64px] xl:text-[80px] xl:text-[50px] ml-2" id="h3_porcentagem">%</span>
           </span>
 
-          <h2 className="text-[clamp(1.09rem,3.95vw,20rem)] xl:text-[clamp(0.992rem,1.48vw,2.5rem)] font-bold w-full mb-4 text-white drop-shadow-xl" id="h2_2">
-            <span className="text-[clamp(1.55rem,5.6vw,20rem)] xl:text-[clamp(1.41rem,2.1vw,3rem)]" id="h2_1">e comece o ano garantindo </span>
+          <h2 className="text-[clamp(1.09rem,3.95vw,20rem)] xl:text-[clamp(0.992rem,1.52vw,2.5rem)] font-bold w-full mb-4 text-white drop-shadow-xl" id="h2_2">
+            <span className="text-[clamp(1.55rem,5.6vw,20rem)] xl:text-[clamp(1.41rem,1.95vw,3rem)]" id="h2_1">e comece o ano garantindo </span>
             <br></br>
             seu sonho de ter o seu diploma!
           </h2>
 
-          <h3 className="text-[clamp(0.81rem,2.94vw,20rem)] xl:text-[clamp(0.736rem,1.1vw,20rem)] mb-6 text-white drop-shadow-xl" id="h3_3">
+          <h3 className="text-[clamp(0.81rem,2.94vw,20rem)] xl:text-[clamp(0.736rem,1.11vw,20rem)] mb-6 text-white drop-shadow-xl" id="h3_3">
             Cursos 100% presenciais desde o 1° Semestre!
           </h3>
 
-          <a href="#form" onClick={handleClick} className="flex items-center justify-center bg-white border-2 border-[#045C3A] text-[clamp(0.95rem,3.7vw,20rem)] md:text-[clamp(0.85rem,3.5vw,20rem)] xl:text-[clamp(0.91rem,0.8vw,1rem)] text-[#045C3A] cursor-pointer rounded-[12px] h-12 md:h-20 xl:h-13 w-[clant(80%, 90%, 100%] sm:w-[90%] xl:w-[25vw] px-6 font-semibold shadow-xl shadow-green-500/40 backdrop-blur transition-all duration-300 hover:bg-[#045C3A] hover:text-white transform hover:-translate-y-1" id="button">
+          <a href="#form" onClick={handleClick} className="flex items-center justify-center bg-white border-2 border-[#045C3A] text-[clamp(0.95rem,3.7vw,20rem)] md:text-[clamp(0.85rem,3.5vw,20rem)] xl:text-[clamp(0.91rem,0.8vw,1rem)] text-[#045C3A] cursor-pointer rounded-[12px] h-12 md:h-20 xl:h-13 w-[clant(80%, 90%, 100%] sm:w-[90%] xl:w-[22.65vw] px-6 font-semibold shadow-xl shadow-green-500/40 backdrop-blur transition-all duration-300 hover:bg-[#045C3A] hover:text-white transform hover:-translate-y-1" id="button">
             QUERO MINHA BOLSA AGORA
           </a>
         </div>
